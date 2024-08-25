e2d9ce481496222c3ff63d2fb7772aa2f4370e65
Cobord <ammar.s.husain@gmail.com>
2024-08-25 19:29:47 -0400

    fmt

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,37 +1,35 @@
-
-
 #[cfg(test)]
 mod test {
     use crabstructor::Constructor;
     #[test]
     fn lib_test() {
-
         #[derive(Constructor, Eq, PartialEq, Debug)]
-        #[constructor(
-            field1 = r#"String::from("test")"#
-        )]
+        #[constructor(field1 = r#"String::from("test")"#)]
         struct Example {
             field1: String,
-            field2: i32
+            field2: i32,
         }
 
-        assert_eq!(Example::new(2), Example {field1: "test".to_string(), field2: 2})
+        assert_eq!(
+            Example::new(2),
+            Example {
+                field1: "test".to_string(),
+                field2: 2
+            }
+        )
     }
 
     #[test]
     fn lib_test_2() {
-
         #[derive(Constructor, Eq, PartialEq, Debug)]
-        #[constructor(
-            field1 = r#"String::from("test")"#
-        )]
+        #[constructor(field1 = r#"String::from("test")"#)]
         struct Example {
             field1: String,
-            field2: i32
+            field2: i32,
         }
 
-        #[derive(PartialEq,Eq,Debug)]
-        struct Foo (bool);
+        #[derive(PartialEq, Eq, Debug)]
+        struct Foo(bool);
         impl Default for Foo {
             fn default() -> Self {
                 Self(true)
@@ -39,15 +37,9 @@ mod test {
         }
 
         #[derive(Constructor, Eq, PartialEq, Debug)]
-        #[constructor(
-            field1 = r#"String::from("test")"#
-        )]
-        #[constructor(
-            field2,field4,field5
-        )]
-        #[constructor(
-            field3 = r#"Example::new(-13)"#
-        )]
+        #[constructor(field1 = r#"String::from("test")"#)]
+        #[constructor(field2, field4, field5)]
+        #[constructor(field3 = r#"Example::new(-13)"#)]
         struct Example2 {
             field1: String,
             field2: i32,
@@ -56,14 +48,26 @@ mod test {
             field5: Foo,
         }
 
-        let expected_nested = Example {field1: "test".to_string(), field2: -13};
-        assert_eq!(Example2::new(), Example2 {field1: "test".to_string(), field2: 0, field3: expected_nested, field4: 0, field5: Foo(true)})
+        let expected_nested = Example {
+            field1: "test".to_string(),
+            field2: -13,
+        };
+        assert_eq!(
+            Example2::new(),
+            Example2 {
+                field1: "test".to_string(),
+                field2: 0,
+                field3: expected_nested,
+                field4: 0,
+                field5: Foo(true)
+            }
+        )
 
-        /// TODO rust-analyzer thinks Example2::new() constructor still takes the field2,field4 and field5 arguments which are using the same
-        /// Type::default() way instead of field_name = ....
-        /// which it does recognize are not part of the constructor
-        /// but the test above still works
-        /// ?????
+        // TODO rust-analyzer thinks Example2::new() constructor still takes the field2,field4 and field5 arguments which are using the same
+        // Type::default() way instead of field_name = ....
+        // which it does recognize are not part of the constructor
+        // but the test above still works
+        // ?????
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Data, Fields, Meta, NestedMeta, Lit};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Lit, Meta, NestedMeta};
 
 #[proc_macro_derive(Constructor, attributes(constructor))]
 pub fn constructor_derive(input: TokenStream) -> TokenStream {
@@ -20,7 +20,8 @@ fn impl_constructor_macro(ast: &DeriveInput) -> TokenStream {
                 let mut constructor_params = Vec::new();
 
                 let mut defaults = std::collections::HashMap::new();
-                let mut default_of_type : std::collections::HashSet<String> = std::collections::HashSet::new();
+                let mut default_of_type: std::collections::HashSet<String> =
+                    std::collections::HashSet::new();
 
                 for attr in &ast.attrs {
                     if attr.path.is_ident("constructor") {
@@ -29,35 +30,41 @@ fn impl_constructor_macro(ast: &DeriveInput) -> TokenStream {
                                 match nested_meta {
                                     NestedMeta::Meta(Meta::NameValue(nv)) => {
                                         if let Lit::Str(lit_str) = &nv.lit {
-                                            defaults.insert(nv.path.get_ident().unwrap().to_string(), lit_str.value());
+                                            defaults.insert(
+                                                nv.path.get_ident().unwrap().to_string(),
+                                                lit_str.value(),
+                                            );
                                         }
-                                    },
+                                    }
                                     NestedMeta::Meta(Meta::Path(nv)) => {
-                                        for this_field in nv.get_ident().unwrap().to_string().split(',') {
+                                        for this_field in
+                                            nv.get_ident().unwrap().to_string().split(',')
+                                        {
                                             default_of_type.insert(this_field.to_string());
                                         }
-                                    },
-                                    _ => {},
-
+                                    }
+                                    _ => {}
                                 }
                             }
                         }
                     }
                 }
-                
+
                 for field in &fields_named.named {
                     let field_name = &field.ident;
                     let field_type = &field.ty;
 
                     let default_value = defaults.get(&field_name.as_ref().unwrap().to_string());
-                    let use_default_of_field_type = default_of_type.contains(&field_name.as_ref().unwrap().to_string());
+                    let use_default_of_field_type =
+                        default_of_type.contains(&field_name.as_ref().unwrap().to_string());
 
                     field_names.push(field_name);
                     field_types.push(field_type);
                     if let Some(value) = default_value {
-                        let value_token: proc_macro2::TokenStream = value.parse().expect("Failed to parse default value");
+                        let value_token: proc_macro2::TokenStream =
+                            value.parse().expect("Failed to parse default value");
                         field_initializers.push(quote! { #field_name: #value_token });
-                    } else if use_default_of_field_type {        
+                    } else if use_default_of_field_type {
                         let value_token: proc_macro2::TokenStream = quote! {#field_type::default()};
                         field_initializers.push(quote! { #field_name: #value_token });
                     } else {
